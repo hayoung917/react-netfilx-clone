3dc230bbb424b68d80d9ad01b014932fcef4c6ee
하영 이 <gkdudp9@naver.com>
2022-03-11 01:26:41 +0900

    네비게이션 바 컴포넌트 생성합니다.

---
+++ src/components/Nav.js
@@ -0,0 +1,37 @@
+import React, { useEffect, useState } from "react";
+import "./Nav.css";
+
+export default function Nav() {
+  const [show, setshow] = useState(false);
+
+  useEffect(() => {
+    window.addEventListener("scroll", () => {
+      console.log("window.scrollY", window.scrollY);
+      if (window.scrollY > 50) {
+        setshow(true);
+      } else {
+        setshow(false);
+      }
+    });
+
+    return () => {
+      window.removeEventListener("scroll", () => {});
+    };
+  }, []);
+
+  return (
+    <nav className={`nav ${show && "nav__black"}`}>
+      <img
+        alt="Netfilx logo"
+        src="https://upload.wikimedia.org/wikipedia/commons/thumb/0/08/Netflix_2015_logo.svg/170px-Netflix_2015_logo.svg.png"
+        className="nav__logo"
+        onClick={() => window.location.reload()}
+      />
+      <img
+        alt="User logged"
+        src="https://upload.wikimedia.org/wikipedia/commons/0/0b/Netflix-avatar.png"
+        className="nav__avatar"
+      />
+    </nav>
+  );
+}
