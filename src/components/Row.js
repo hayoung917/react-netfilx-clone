3c0f55fe7ebc63d9a6feba7fddbc291df1ed1f03
하영 이 <gkdudp9@naver.com>
2022-03-14 23:44:35 +0900

    슬라이드 기능 추가합니다.

--- src/components/Row.js
+++ src/components/Row.js
@@ -21,7 +21,14 @@ export default function Row({ isLargeRow, title, fetchUrl, id }) {
       <h2>{title}</h2>
       <div className="slider">
         <div className="slider__arrow-left">
-          <span className="arrow">{"<"}</span>
+          <span
+            className="arrow"
+            onClick={() => {
+              document.getElementById(id).scrollLeft -= window.innerWidth - 80;
+            }}
+          >
+            {"<"}
+          </span>
         </div>
         <div id={id} className="row__posters">
           {movies.map((movie) => (
@@ -30,13 +37,20 @@ export default function Row({ isLargeRow, title, fetchUrl, id }) {
               className={`row__poster ${isLargeRow && "row__posterLarge"}`}
               src={`https://image.tmdb.org/t/p/original/${
                 isLargeRow ? movie.poster_path : movie.backdrop_path
-              }`}
+              } `}
               alt={movie.name}
-            ></img>
+            />
           ))}
         </div>
         <div className="slider__arrow-right">
-          <span className="arrow">{">"}</span>
+          <span
+            className="arrow"
+            onClick={() => {
+              document.getElementById(id).scrollLeft += window.innerWidth - 80;
+            }}
+          >
+            {">"}
+          </span>
         </div>
       </div>
     </section>
