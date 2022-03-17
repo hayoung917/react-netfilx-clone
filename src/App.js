f266f4da7f6b10f11b84571f4f115816752d61b0
하영 이 <gkdudp9@naver.com>
2022-03-17 23:31:16 +0900

    Search Page Route의 path수정합니다.

--- src/App.js
+++ src/App.js
@@ -25,7 +25,7 @@ function App() {
         <Route path="/" element={<Layout />}>
           <Route index element={<MainPage />} />
           <Route path=":movieId" element={<DetailPage />} />
-          <Route path element={<SearchPage />} />
+          <Route path="search" element={<SearchPage />} />
         </Route>
       </Routes>
     </div>
