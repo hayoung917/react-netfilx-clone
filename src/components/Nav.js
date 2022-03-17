559946372cde4679a0c1eec9453cf7e0aaf088e6
하영 이 <gkdudp9@naver.com>
2022-03-17 23:31:50 +0900

    useLocation을 사용하여 검색페이지 구현합니다.

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,8 +1,11 @@
 import React, { useEffect, useState } from "react";
+import { useNavigate } from "react-router-dom";
 import "./Nav.css";
 
 export default function Nav() {
   const [show, setshow] = useState(false);
+  const [searchValue, setSearchValue] = useState("");
+  const navigate = useNavigate();
 
   useEffect(() => {
     window.addEventListener("scroll", () => {
@@ -19,6 +22,11 @@ export default function Nav() {
     };
   }, []);
 
+  const handleChange = (e) => {
+    setSearchValue(e.target.value);
+    navigate(`/search?q=${e.target.value}`);
+  };
+
   return (
     <nav className={`nav ${show && "nav__black"}`}>
       <img
@@ -27,6 +35,15 @@ export default function Nav() {
         className="nav__logo"
         onClick={() => window.location.reload()}
       />
+
+      <input
+        value={searchValue}
+        onChange={handleChange}
+        className="nav__input"
+        type="text"
+        placeholder="영화를 검색해주세요."
+      />
+
       <img
         alt="User logged"
         src="https://upload.wikimedia.org/wikipedia/commons/0/0b/Netflix-avatar.png"
