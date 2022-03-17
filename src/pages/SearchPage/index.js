1dc95841905bbb3a89d45aa0ef114d2d45bb3724
하영 이 <gkdudp9@naver.com>
2022-03-17 23:55:38 +0900

    useDebounce custom hook을 만듭니다.

--- src/pages/SearchPage/index.js
+++ src/pages/SearchPage/index.js
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from "react";
 import { useLocation } from "react-router-dom";
 import axios from "../../api/axios";
+import { useDebounce } from "../../hooks/useDebounce";
 import "./SearchPage.css";
 
 export default function SearchPage() {
@@ -13,13 +14,21 @@ export default function SearchPage() {
 
   let query = useQuery();
   const searchTerm = query.get("q");
-  console.log("serchTerm", searchTerm);
+  const debouncedSearchTerm = useDebounce(searchTerm, 500);
+  // console.log("serchTerm", searchTerm);
+  console.log("debouncedSearchTerm", debouncedSearchTerm);
+
+  // useEffect(() => {
+  //   if (searchTerm) {
+  //     fetchSearchMovie(searchTerm);
+  //   }
+  // }, [searchTerm]);
 
   useEffect(() => {
-    if (searchTerm) {
-      fetchSearchMovie(searchTerm);
+    if (debouncedSearchTerm) {
+      fetchSearchMovie(debouncedSearchTerm);
     }
-  }, [searchTerm]);
+  }, [debouncedSearchTerm]);
 
   const fetchSearchMovie = async (searchTerm) => {
     try {
@@ -41,7 +50,7 @@ export default function SearchPage() {
             const movieImageUrl =
               "https://image.tmdb.org/t/p/w500" + movie.backdrop_path;
             return (
-              <div className="movie">
+              <div className="movie" key={movie.id}>
                 <div className="movie__column-poster">
                   <img
                     src={movieImageUrl}
@@ -58,6 +67,7 @@ export default function SearchPage() {
       <section className="no-results">
         <div className="no-results__text">
           <p>찾고자하는 검색어 "{searchTerm}"에 맞는 영화가 없습니다.</p>
+          {/* <p>찾고자하는 검색어 "{searchTerm}"에 맞는 영화가 없습니다.</p> */}
         </div>
       </section>
     );
