349e3ca09e90c08df34ed2d37c5e8cb0bf2c059f
하영 이 <hy.lee@saigeresearch.ai>
2022-03-22 22:56:00 +0900

    영화 검색 후 netfilx버튼 누를 시 홈으로 이동하지 않던 버그 해결합니다.

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -27,13 +27,18 @@ export default function Nav() {
     navigate(`/search?q=${e.target.value}`);
   };
 
+  const handleRefresh = () => {
+    setSearchValue("");
+    navigate(`/`)
+  };
+
   return (
     <nav className={`nav ${show && "nav__black"}`}>
       <img
         alt="Netfilx logo"
         src="https://upload.wikimedia.org/wikipedia/commons/thumb/0/08/Netflix_2015_logo.svg/170px-Netflix_2015_logo.svg.png"
         className="nav__logo"
-        onClick={() => window.location.reload()}
+        onClick={handleRefresh}
       />
 
       <input
