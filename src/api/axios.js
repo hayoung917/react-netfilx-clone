b8364c40b5e4863de6bd93ebe9f2d797916d6b34
하영 이 <gkdudp9@naver.com>
2022-03-26 19:10:11 +0900

    API KEY 환경변수로 숨깁니다.

--- src/api/axios.js
+++ src/api/axios.js
@@ -3,7 +3,7 @@ import axios from "axios";
 const instance = axios.create({
   baseURL: "https://api.themoviedb.org/3",
   params: {
-    api_key: "641d7821f1c60ede729e6181457baa09",
+    api_key: process.env.REACT_APP_MOVIE_DB_API_KEY,
     language: "ko-KR",
   },
 });
